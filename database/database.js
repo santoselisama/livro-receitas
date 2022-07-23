04c5ba57f81fff1ccab0f95fb235a9a8e94b0a81
Sabrina <sabrinaxdtmk@hotmail.com>
2022-07-22 21:03:38 -0300

    atualização da tela de receita

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 const sequelize = require ('sequelize');
-const connection = new sequelize('projeto','sabrina','elisamasabrina123', {
+const connection = new sequelize('projeto','root','root', {
     host:'localhost',
     dialect:'mysql'
 });
